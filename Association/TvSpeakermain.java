2300c2a22f72aaf56dc6d5e1d674ba3b7459e82d
Sindhu-s20 <sindhus.xworkz@gmail.com>
2023-11-06 11:48:04 +0530

    con

---
+++ Association/TvSpeakermain.java
@@ -0,0 +1,15 @@
+class TvSpeakermain{
+		
+		public static void main(String args[])
+		{
+		 System.out.println("starting in main method");
+		 Speaker speaker=new Speaker(200);
+		 Tv tv=new Tv(speaker);
+		 tv.displayInfo();
+		  System.out.println("starting in main method");
+		 
+		 }
+		 } 
+		 
+		 
+		
