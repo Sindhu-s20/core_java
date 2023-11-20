9483790c21571922dd9283ffd9a1dca2aaff245e
Sindhu-s20 <sindhus.xworkz@gmail.com>
2023-11-20 23:10:33 +0530

    usinginheritance

---
+++ inheritance/main.java
@@ -0,0 +1,67 @@
+class Main
+{
+	public static void main(String[] args)
+	{
+		Food1 food=new Food1();
+		
+		System.out.println("===========");
+		
+		Sweet sweet=new Sweet();
+		
+		System.out.println("===========");
+		
+		Food1 food1=new Sweet();
+		
+		System.out.println("===========");
+		
+		Sweet sweet1=new Ladoo();
+
+		System.out.println("===========");
+		
+		Ladoo ladoo=new MotichuruLadoo();
+		
+		System.out.println("===========");
+		
+		Food1 food2=new Biscuit();
+		
+		System.out.println("===========");
+		
+		Biscuit bicuit=new Tiger();
+				
+		System.out.println("===========");
+		
+		Tiger tiger=new SpecialTiger();
+		
+		System.out.println("===========");
+		
+		Tiger tiger1=new GlucoTiger();
+		
+		System.out.println("===========");
+		
+		Biscuit biscuit1=new Parle();
+		
+		System.out.println("===========");
+		
+		Parle parle2=new GlucoParle();
+		
+		System.out.println("===========");
+		
+		Food1 food3=new Ladoo();
+		
+		System.out.println("===========");
+		
+		Food1 food4=new MotichuruLadoo();
+		
+		System.out.println("===========");
+		
+		Biscuit biscuit2=new SpecialTiger();
+		
+		System.out.println("===========");
+		
+		Food1 food5=new GlucoParle();
+		
+		System.out.println("===========");
+		
+		Food1 food6=new GlucoTiger();
+			}
+}
