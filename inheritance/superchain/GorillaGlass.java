7197c7d2d17b5c9a1fc1594463ca6a602b3243c3
Sindhu-s20 <sindhus.xworkz@gmail.com>
2023-11-21 23:39:03 +0530

    using chaing

---
+++ inheritance/superchain/GorillaGlass.java
@@ -0,0 +1,11 @@
+class GorillaGlass extends LaminatedGlass{
+		
+		GorillaGlass()
+		{
+			super(800.0,5);
+		System.out.println("this is default constructor no-argument of GorillaGlass");
+			
+			
+		}
+		
+}
