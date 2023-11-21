67fcce2660c50dbfca02392e6ab5ba3be18a18b2
Sindhu-s20 <sindhus.xworkz@gmail.com>
2023-11-21 23:36:55 +0530

    using chaing

---
+++ inheritance/superchain/DigitalCamera.java
@@ -0,0 +1,8 @@
+class DigitalCamera extends Camera{
+	
+	DigitalCamera()
+	{
+		super("cano",3.0,CameraColor.BLUE); //chaining 
+		System.out.println("default constructor with no-argument DigitalCamera");
+	}
+}
