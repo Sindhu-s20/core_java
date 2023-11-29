63670f863d52880a7436529510db719ba6133dc8
Sindhu-s20 <sindhus.xworkz@gmail.com>
2023-11-29 11:38:35 +0530

    package

---
+++ inteligent/orinaments/src/com/Xworkz/orinaments.java
@@ -0,0 +1,25 @@
+package com.Xworkz;
+
+public class orinaments {
+    public orinaments() {
+        System.out.println("this is no-arg constructor of orinaments");
+                }
+                public void color()
+                {
+                    System.out.println("orinaments have gold color");
+                }
+    private void Gold() {
+        System.out.println("this is gold with private method");
+    }
+
+    void display() {
+        System.out.println("this is default method");
+    }
+    protected void silver() {
+        System.out.println("this is protected method");
+    }
+    public void platinum()
+    {
+        System.out.println("this is public class");
+    }
+}
